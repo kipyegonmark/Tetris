2f9645550cc30a65f53be074253bb62bc31b18a3
Mark Kipyegon <kipyegonmark@protonmail.ch>
2016-03-16 15:39:48 +0300

    Added function to implement Tetris blocks

---
+++ Tetris/Tetris/Block.swift
@@ -0,0 +1,76 @@
+//
+//  Block.swift
+//  Tetris
+//
+//  Created by Mark Kipyegon Koskei on 16/03/2016.
+//
+//
+
+import SpriteKit
+
+let NumberOfColors: UInt32 = 6
+
+enum BlockColor: Int, CustomStringConvertible {
+    
+    case Blue = 0, Orange, Purple, Red, Teal, Yellow
+    
+    var spriteName: String {
+        switch self {
+        case .Blue:
+            return "blue"
+        case .Orange:
+            return "orange"
+        case .Purple:
+            return "purple"
+        case .Red:
+            return "red"
+        case .Teal:
+            return "teal"
+        case .Yellow:
+            return "yellow"
+        }
+    }
+    
+    var description: String {
+        return self.spriteName
+    }
+    
+    static func random() -> BlockColor {
+        return BlockColor(rawValue:Int(arc4random_uniform(NumberOfColors)))!
+    }
+}
+
+// Declare a class to implement the protocols Hashable and CustomStringConvertible
+class Block: Hashable, CustomStringConvertible {
+    
+    // Define the property color
+    let color: BlockColor
+    
+    // Declare the properties which will represent a Block
+    var column: Int
+    var row: Int
+    var sprite: SKSpriteNode?
+    
+    var spriteName: String {
+        return color.spriteName
+    }
+    
+    var hashValue: Int {
+        return self.column ^ self.row
+    }
+    
+    var description: String {
+        return "\(color): [\(column), \(row)]"
+    }
+    
+    init(column:Int, row:Int, color:BlockColor) {
+        self.column = column
+        self.row = row
+        self.color = color
+    }
+}
+
+func ==(lhs: Block, rhs: Block) -> Bool {
+    return lhs.column == rhs.column && lhs.row == rhs.row && lhs.color.rawValue == rhs.color.rawValue
+
+}
