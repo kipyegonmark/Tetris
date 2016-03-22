166599524d4a16b384f3a494bdb179954313ecbd
Mark Kipyegon <kipyegonmark@protonmail.ch>
2016-03-22 09:21:56 +0300

    Added subclasses to define the different shapes

---
+++ Tetris/Tetris/LineShape.swift
@@ -0,0 +1,27 @@
+//
+//  LineShape.swift
+//  Tetris
+//
+//  Created by Mark Kipyegon Koskei on 22/03/2016.
+//
+//
+
+class LineShape:Shape {
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [
+            Orientation.Zero:       [(0, 0), (0, 1), (0, 2), (0, 3)],
+            Orientation.Ninety:     [(-1,0), (0, 0), (1, 0), (2, 0)],
+            Orientation.OneEighty:  [(0, 0), (0, 1), (0, 2), (0, 3)],
+            Orientation.TwoSeventy: [(-1,0), (0, 0), (1, 0), (2, 0)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[FourthBlockIdx]],
+            Orientation.Ninety:     blocks,
+            Orientation.OneEighty:  [blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: blocks
+        ]
+    }
+}
